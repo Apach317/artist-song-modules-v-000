012f7ddfe35a312420927703224ad6eb7ba6b913
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-28 05:30:18 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,3 +1,17 @@
 module Memorable
+  def self.reset_all
+    self.all.clear
+  end
 
+  def self.count
+    self.all.count
+  end
+end
+
+class Artist
+  extend Memorable::ClassMethods
+end
+
+class Song
+  extend Memorable::ClassMethods
 end
