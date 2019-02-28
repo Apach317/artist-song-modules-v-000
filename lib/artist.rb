9635365bca2ba2d27408312c617adbbaf7b5ebde
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-28 05:52:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,4 @@
 require 'pry'
-# require_relative '../lib/concerns/memorable'
 
 class Artist
   attr_accessor :name
@@ -12,9 +11,6 @@ class Artist
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@artists
@@ -29,7 +25,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
